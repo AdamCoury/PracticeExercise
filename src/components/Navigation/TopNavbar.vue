54d38fe242a7ec68b8a009549dc1614f191a4c0b
Adam <fy5219@wayne.edu>
2020-05-21 21:35:07 -0400

    Final styling pages

--- src/components/Navigation/TopNavbar.vue
+++ src/components/Navigation/TopNavbar.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
     <div
-      class="navbar navbar-expand-lg navbar-light bg-light h-100 m-0 pt-0 pb-0 h-100"
+      class="navbar navbar-expand-lg  h-100 m-0 pt-0 pb-0 h-100"
+      style="background-color: #961A1A"
     >
       <div class="container ml-1 justify-content-start">
-        <a class="navbar-brand pt-0 pb-0">ESPORTS ENGINE</a>
+        <a class="navbar-brand pt-0 pb-0" style="color: white">ESPORTS ENGINE</a>
         <ul class="navbar-nav w-25">
           <li :class="{'background': $route.name==='Home'}" class="nav-item active mr-2 w-50 pt-2 pb-2">
-            <router-link class="routeItem" to="/">Home</router-link>
+            <router-link :class="{'color-red': $route.name==='Home', 'color-white': $route.name!=='Home'}" class="routeItem" to="/">Home</router-link>
           </li>
           <li :class="{'background': $route.name==='Our Team'}" class="nav-item active ml-2 w-50 pt-2 pb-2">
-            <router-link class="routeItem" to="/our-team">Our Team</router-link>
+            <router-link class="routeItem" to="/our-team" :class="{'color-red': $route.name==='Our Team', 'color-white': $route.name!=='Our Team'}">Our Team</router-link>
           </li>
         </ul>
       </div>
@@ -34,5 +35,13 @@ body {
 
   .background {
     background-color: lightgray;
+  }
+
+  .color-red{
+    color: #961A1A;
+  }
+
+  .color-white {
+    color: white;
   }
 </style>
